18643b09e67a9faa927fcee1c727a2d02a9b9599
Celfox <seariekaru@gmail.com>
2018-07-08 08:15:09 -0500

    Actual initial commit
    This is the first publicly visible version of clem. Clem is terrible. I'm sorry.

---
+++ app.js
@@ -0,0 +1,104 @@
+const Items = require('warframe-items')
+const Discord = require('discord.js')
+const {Signale} = require('signale')
+const clem = require('./lib')
+const client = new Discord.Client()
+const items = new Items()
+const fuzzy = require('fuzzy')
+const request = require('request')
+const config = require('./config.json')
+
+const channel_id = ""
+
+const signale_options = {
+  disabled: false,
+  interactive: false,
+  stream: process.stdout,
+  scope: 'clem'
+}
+
+const logger = new Signale(signale_options)
+
+
+var previous_alerts = []
+
+var search_template = {
+    extract: function(entry) {
+	return entry.name
+    }
+}
+
+
+function f_search(target) {
+	var results = fuzzy.filter(target,items,search_template)
+	return results.map(function(x) { return x.original })
+}
+
+function push_channel(channel, message) {
+    client.channels.get(channel).send(message)
+}
+
+
+function get_worldstate_silently() {
+    request('https://ws.warframestat.us/pc', function (error, response, body) {
+	if(error) {
+	    logger.error(error)
+	}
+	clem.process_alerts(JSON.parse(body).alerts, true, logger)
+    })
+
+}
+
+function get_worldstate() {
+    request('https://ws.warframestat.us/pc', function (error, response, body) {
+	if(error) {
+	    logger.error(error)
+	}
+	var alerts = clem.process_alerts(JSON.parse(body).alerts, true, logger)
+	if(alerts.length > 0) {
+	    push_channel(config.channel_id,alerts.join("\n"))
+	}
+    })
+
+}
+
+client.on('ready', () => {
+    logger.info(`Clem has been gotten: ${client.user.tag}`)
+    setInterval(get_worldstate, 600000)
+    get_worldstate_silently()
+})
+
+client.on('message', msg => { 
+    var content = msg.content.split(' ')
+    var command = content[0]
+    var predicate = content.slice(1,content.length).join(' ')
+    if(msg.channel.id === config.channel_id) {
+	switch (command) {
+	case ".search": 
+	    logger.info("Searching for " + predicate)
+	    var query = f_search(predicate)
+	    logger.info(`... got ${query.length} results.`)
+	    var res = clem.transform_results(query)
+	    if(res.length > 10) {
+		res = res.slice(0,9)
+	    }
+	    for(var entry of res) {
+		msg.reply(entry)
+	    }
+	    break
+	case ".alerts":
+	    request('https://ws.warframestat.us/pc', function (error, response, body) {
+		 if(error) {
+		     logger.error(error)
+		 }
+		 var alerts = process_alerts(JSON.parse(body).alerts, false)
+		 if(alerts.length > 0) {
+		     push_channel(channel_id,alerts.join("\n"))
+		 }
+	     })
+	    break
+	}
+    }
+})
+
+client.login(config.token)
---
+++ lib.js
@@ -0,0 +1,156 @@
+const _ = require('lodash/array')
+const duration = require('human-duration')
+var previous_alerts = []
+
+function parse_components(components) { 
+    var out = []
+    for(var component of components) {
+	if('drops' in component)  {
+	    var drops = parse_drops_brevity(component.drops)
+	    out.push(`${component.itemCount} ${component.name} \n ${drops}`)
+	    
+	} else {
+	    out.push(`${component.itemCount} ${component.name}`)
+	}
+    }
+    return out.join('\n')
+}
+function parse_drops(drops) {
+    var out = []
+    for(var drop of drops) {
+	out.push(`${drop.location} as a ${drop.type} at a chance of ${drop.chance * 100}%`)
+    }
+    return out.join('\n')
+}
+function parse_drops_brevity(drops) {
+    var out = []
+    for(var drop of drops) {
+	out.push(`${drop.location} ${drop.chance * 100}%`)
+    }
+    return out.join('\n')
+}    
+
+function precise(x) {
+    return Number.parseFloat(x).toPrecision(4);
+}
+function process_alert(alert) {
+    var out = `${alert.mission.type} on ${alert.mission.node} for ${alert.mission.reward.asString}`
+    if(alert.mission.reward.asString.includes("Nitain")) {
+	out = "🚨 " + out + " 🚨"
+    } else {
+	out = "⚠ " + out
+    }
+    return out
+}
+
+module.exports = {
+    process_alerts: function process_alerts(data, diff, logger) {
+	var out = []	
+
+	if(diff) {
+	    var alerts = data.map(xs => xs.id)
+	    var new_alerts = _.difference(alerts,previous_alerts)
+	    logger.info(new_alerts)
+	    previous_alerts = alerts
+	    var correct_data = data.filter(xs => new_alerts.includes(xs.id))
+	    for(var alert of correct_data) {
+		out.push(process_alert(alert))
+	    }
+	} else {
+	    for(var alert of data) {
+		out.push(process_alert(alert))
+	    }
+	}
+	return out
+    },
+
+    transform_results: function transform_results(res) {
+	var out = []
+	for(var item of res) {
+	    var com = parse_components(item.components || [])
+	    var drops = parse_drops(item.drops || [])
+	    if(item.category === 'Melee') {
+		out.push( ` **${item.name}** 
+\`\`\`
+${item.description}
+Mastery requirement: ${item.masteryReq || "N/A"}
+Base damage: ${item.totalDamage}
+DPS: ${item.damagePerSecond}
+Critical chance: ${precise(item.criticalChance * 100)}%
+Critical multiplier: ${precise(item.criticalMultiplier)}x
+Proc chance: ${precise(item.procChance)*100}%
+Swing rate: ${precise(item.fireRate)} (swings per second)
+Build time: ${duration.fmt(item.buildTime*1000 || 0)}
+Build price: ${item.buildPrice || 0} cr
+${com.length > 0 ? "Components:\n" + com : ""}
+\`\`\` `)
+	    } else if(item.category === "Secondary" || item.category === "Primary") {
+		out.push( ` **${item.name}** 
+\`\`\`
+${item.description}
+Mastery requirement: ${item.masteryReq || "N/A"}
+Base damage: ${item.totalDamage}
+DPS: ${item.damagePerSecond}
+Critical chance: ${precise(item.criticalChance * 100)}%
+Critical multiplier: ${precise(item.criticalMultiplier)}x
+Proc chance: ${precise(item.procChance)*100}%
+Fire rate: ${precise(item.fireRate)} (rounds per second)
+Accuracy: ${item.accuracy || "N/A"}%
+Magazine size: ${item.magazineSize || "N/A"}
+Build time: ${duration.fmt(item.buildTime*1000 || "N/A")}
+Build price: ${item.buildPrice || "N/A"} cr
+${com.length > 0 ? "Components:\n" + com : ""}
+\`\`\` `)
+	    } else if(item.category === "Resources") {
+		
+	    	out.push( ` **${item.name}** 
+\`\`\`
+${item.description}
+${com.length > 0 ? "Components:\n" + com : ""}
+${drops.length > 0 ? "Drops:\n" + drops : ""}
+Build time: ${duration.fmt(item.buildTime*1000 || "N/A") || "N/A"}
+Build price: ${item.buildPrice || "N/A"} cr
+\`\`\` `)
+	    } else if(item.category === "Mods") {
+		
+	    	out.push( ` **${item.name}** 
+\`\`\`
+Rarity: ${item.rarity || "N/A"}
+Polarity: ${item.polarity || "N/A"}
+${drops.length > 0 ? "Drops:\n" + drops : ""}
+
+\`\`\` `)
+	    }  else if(item.category === "Warframes") {
+	    	out.push( ` **${item.name}** 
+\`\`\`
+${item.description}
+${com.length > 0 ? "Components:\n" + com : ""}
+${drops.length > 0 ? "Drops:\n" + drops : ""}
+Build time: ${duration.fmt(item.buildTime*1000 || "N/A") || "N/A"}
+Build price: ${item.buildPrice || "N/A"} cr
+
+\`\`\` `)
+	    } else if(item.category === "Relics") {
+		var drops = "Vaulted" // damn you, lith k1
+		if('drops' in item) {
+		    drops = parse_drops_brevity(item.drops.slice(0,30) || [])
+		}
+		if(drops != "Vaulted")  {
+	    	out.push( ` **${item.name}** 
+${item.description}
+\`\`\`
+${drops.length > 0 ? "Drops:\n" + drops : ""}
+\`\`\` `) 
+		} else {
+		    	out.push( ` ~~${item.name}~~
+
+\`\`\`
+This relic is vaulted and no longer available
+\`\`\` `) 
+		}
+		break
+	    }  
+	}
+	return out
+    } 
+}
